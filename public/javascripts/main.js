ab17c3d1c222bfe3e5846e849e5867eb9883e6e4
qinxc <qinxc@yomob.com>
2017-12-29 17:44:04 +0800

    label create

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -33,31 +33,50 @@ $(document).ready(function(){
 
 });
 
-var game_method = new Array("MMORPG","SLG","卡牌","竞技","策略","放置","经营","消除","竞速",
-                            "赌博","棋牌","生存","单机","沙盒","IO","MOBA","射击","冒险","体育",
-                            "模拟","街机","益智","音乐","文字");
-markDiv = document.getElementsByClassName("mark-group")[0];
-h4 = document.createElement("h4");
-h4.innerHTML = "游戏";
-h5 = document.createElement("h5");
-h5.innerHTML = "玩法";
-h4.appendChild(h5);
-markDiv.appendChild(h4);
 
-table = document.createElement("table");
-tbody = document.createElement("tbody");
+$(document).ready(function(){
 
-var line_num = 5;
+    var game_method = new Array("MMORPG","SLG","卡牌","竞技","策略","放置","经营","消除","竞速",
+                                "赌博","棋牌","生存","单机","沙盒","IO","MOBA","射击","冒险","体育",
+                                "模拟","街机","益智","音乐","文字");
 
-for(i=0;i<game_method.length;i++){
-    if(i%line_num == 0){
-        tr = document.createElement("tr");
+    markDiv = document.getElementsByClassName("mark-group")[0];
+    h4 = document.createElement("h4");
+    h4.innerHTML = "游戏";
+    h5 = document.createElement("h5");
+    h5.innerHTML = "玩法";
+    h4.appendChild(h5);
+    markDiv.appendChild(h4);
+
+    table = document.createElement("table");
+    tbody = document.createElement("tbody");
+    tr = document.createElement("tr");
+
+    var line_num = 5;
+
+    for(i=0;i<game_method.length;i++){
+        if(i%line_num == 0){
+            tbody.appendChild(tr);
+            tr = document.createElement("tr");
+        }
+        else{
+            td = document.createElement("td");
+            label = document.createElement("label");
+            input = document.createElement("input");
+            input.class = "markbox";
+            input.type = "checkbox";
+            input.name = "markbox"
+            label.appendChild(input);
+            label.innerHTML = game_method[i];
+            td.appendChild(label);
+            tr.appendChild(td);
+        }
     }
-    else{
-        td = document.createElement("td");
-        label = document.createElement();
-    }
-}
+    tbody.appendChild(tr);
+    table.appendChild(tbody);
+    markDiv.appendChild(table);
+});
+
 
 
 
